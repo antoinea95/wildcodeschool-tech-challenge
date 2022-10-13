167377aec7b2f251233effc7b6466b7af55bb4ee
Antoine <102359550+antoinea95@users.noreply.github.com>
2022-10-13 15:21:38 +0200

    create node app

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const helmet = require("helmet");
+
+// import de la connexion a mongoDB
+require("./config/mongoDb");
+
+// import router
+
+const argonautesRoute = require('./routes/argonautes');
+
+const app = express();
+
+app.use(helmet());
+
+/* Headers permettent d'accéder à l'API depuis n'importe quelle orgine, d'ajouter les headers mentionnées 
+et de formuler les requêtes avec les méthodes mentionnées */
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Cross-Origin-Resource-Policy", "same-site");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+  });
+
+
+app.use(express.json());
+app.use(argonautesRoute);
+module.exports = app;
