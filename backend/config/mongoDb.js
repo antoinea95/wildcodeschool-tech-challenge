b5b20cffb4113ced2c29e368dae3a1bb1a7bf6f2
Antoine <102359550+antoinea95@users.noreply.github.com>
2022-10-13 15:22:01 +0200

    config mongoDb

---
+++ backend/config/mongoDb.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+
+// connexion à la base de donnée mongoDB
+mongoose
+    .connect(`${process.env.MONGO_DB_URL}`,
+    { useNewUrlParser: true,
+    useUnifiedTopology: true })
+      .then(() => console.log('Connexion à MongoDB réussie !'))
+      .catch(() => console.log('Connexion à MongoDB échouée !'));
